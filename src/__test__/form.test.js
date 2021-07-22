51e2ee6f03d6649348f8257f13a628aa796a65f4
Louis Lassegue <louislassegue@gmail.com>
2021-07-21 17:01:54 -0700

    updated test

--- src/__test__/form.test.js
+++ src/__test__/form.test.js
@@ -2,11 +2,18 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import App from '../app'
+describe('test the App component', () => {
+  test('Assert that upon submitting the form will result in data being rendered in the output area', async () => {
 
-test('Assert that upon submitting the form will result in data being rendered in the output area', async()=> {
-render(<App/>);
-const button = screen.getByTestId('formSubmit');
-const data = screen.getByTestId('resultsData');
-fireEvent.click(button);
-expect(data).toHaveTextContent('')
-})
+    // See a UI, needs a form
+    render(<App />);// show the UI on the screen
+
+    // UI should have a submission button
+    const button = screen.getByTestId('formSubmit');
+    const data = screen.getByTestId('resultsData');
+console.log(data);
+    // regardless of input, button is clicked and data is rendered
+    fireEvent.click(button);
+    expect(data).toHaveTextContent('');
+  });
+});
