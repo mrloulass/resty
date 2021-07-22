d6dbfcb17f86e9197db1a9832e52dbf915aa3dd2
Louis Lassegue <louislassegue@gmail.com>
2021-07-21 21:33:37 -0700

    added test for fake api request

--- README.md
+++ README.md
@@ -4,6 +4,19 @@
 
 ## Setup
 
+In the project directory, you can run:
+
+`npm start`
+- Runs the app in the development mode.
+- Open http://localhost:3000 to view it in the browser.
+
+- The page will reload if you make edits.
+- You will also see any lint errors in the console.
+
+`npm test`
+- Launches the test runner in the interactive watch mode.
+- See the section about running tests for more information.
+
 ## Development Process
 
 ### Phase 1: Application Setup
@@ -41,6 +54,11 @@
 
 ## Tests
 
+- test the App component
+  - upon submitting the form will result in data being rendered in the output area
+
+- Testing API effect
+  - app should fetch
 ## UML
 
 - Rest
--- src/__test__/form.test.js
+++ src/__test__/form.test.js
@@ -1,7 +1,29 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-
+import React from 'react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import App from '../app'
+
+const server = setupServer(
+  rest.get('https://pokeapi.co/api/v2/pokemon/', (req, res, ctx) => {
+    return res(ctx.json({ results: [{ name: "test" }] }));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe('Testing API effect', () => {
+  test('app should fetch', async () => {
+    render(<App />);
+
+    let data = await waitFor(() => screen.getByTestId('method'));
+    expect(data).toHaveTextContent('GET');
+  });
+});
+
 describe('test the App component', () => {
   test('Assert that upon submitting the form will result in data being rendered in the output area', async () => {
 
@@ -11,7 +33,7 @@ describe('test the App component', () => {
     // UI should have a submission button
     const button = screen.getByTestId('formSubmit');
     const data = screen.getByTestId('resultsData');
-console.log(data);
+    
     // regardless of input, button is clicked and data is rendered
     fireEvent.click(button);
     expect(data).toHaveTextContent('');
